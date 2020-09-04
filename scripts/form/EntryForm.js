009f2b7ec9276370fa2ce6aae31ed3863d573fec
ste163 <samtedwards@pm.me>
2020-09-04 16:14:08 -0400

    added eventlistener to save button. Needs save function

--- scripts/form/EntryForm.js
+++ scripts/form/EntryForm.js
@@ -1,24 +1,53 @@
+// Need to import a saveEntry function from the provider.
+const eventHub = document.querySelector(".gridMain");
 const entryDOMtarget = document.querySelector(".sp-form");
+
+//need an event listener on the save button
+eventHub.addEventListener("click", e => {
+    if (e.target.id === "new__save-btn") {
+        console.log("HIT SAVE BUTTON");
+
+        const entryAuthor = document.querySelector(".new__author--text");
+        const entryDate = document.querySelector(".new__date--input");
+        const entryMood = document.querySelector(".new__mood--dropdown");
+        const entryConcepts = document.querySelector(".new__concept--text");
+        const entryTitle = document.querySelector(".new__title--text");
+        const entryText = document.querySelector(".new__text--text");
+
+        const newEntry = {
+            date: entryDate.value,
+            author: entryAuthor.value,
+            mood: entryMood.value,
+            concept: entryConcepts.value,
+            title: entryTitle.value,
+            text: entryText.value
+        }
+    }
+})
 
 export const renderEntryForm = () => {
     entryDOMtarget.innerHTML = `
     <h2 class="sp-form__h2">Create a new Spiral</h2>
     <form class="new" action="">
-            <fieldset class="new__center">
-                <label class="new__label new__label--small" for="s-date">Date Written</label>
-                <input class="new__date--input" type="date" id="s-date" name="s-date">
-            </fieldset>
-            <fieldset class="new__center">
-                <label class="new__label new__label--small" for="s-mood">How's your Mood?</label>
-                <select class="new__mood--dropdown" id="s-mood" name="s-mood">
-                    <option value="fantastic">Fantastic</option>
-                    <option value="groovy">Groovy</option>
-                    <option value="funky">Funky</option>
-                    <option value="confused">Confused</option>
-                    <option value="annoyed">Annoyed</option>
-                    <option value="don't-ask">Don't Ask</option>
-                </select>
-            </fieldset>
+        <fieldset class="new__center">
+           <label class="new__label new__label--small" for="s-author">Author</label>
+           <textarea class="new__author--text" name="s-author" rows="1" placeholder="Who are you?"></textarea>
+        </fieldset>
+        <fieldset class="new__center">
+            <label class="new__label new__label--small" for="s-date">Date Written</label>
+            <input class="new__date--input" type="date" id="s-date" name="s-date">
+        </fieldset>
+        <fieldset class="new__center">
+            <label class="new__label new__label--small" for="s-mood">How's your Mood?</label>
+            <select class="new__mood--dropdown" id="s-mood" name="s-mood">
+                <option value="fantastic">Fantastic</option>
+                <option value="groovy">Groovy</option>
+                <option value="funky">Funky</option>
+                <option value="confused">Confused</option>
+                <option value="annoyed">Annoyed</option>
+                <option value="don't-ask">Don't Ask</option>
+            </select>
+        </fieldset>
         <fieldset class="new__center">
             <label class="new__label" for="s-concept" id="s-concept" name="s-concept">Concepts Covered</label>
             <textarea class="new__concept--text" name="s-concept" rows="2" placeholder="JavaScript, HTML, CSS, APIs"></textarea>
@@ -31,7 +60,7 @@ export const renderEntryForm = () => {
                 <label class="new__label" for="s-text" id="s-text" name="s-text">Text</label>
                 <textarea class="new__text--text" name="s-text" rows="8" placeholder="You can save all sorts of data."></textarea>
         </fieldset>
-        <input class="new__save-btn" type="submit" value="Save Spiral">
+        <input id="new__save-btn" class="new__save-btn" type="submit" value="Save Spiral">
     </form>
     `
 }
