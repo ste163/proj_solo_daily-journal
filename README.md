033034449494c5d277d05c4ca3c0ed7b483a073b
ste163 <samtedwards@pm.me>
2020-09-04 11:16:43 -0400

    accidentally worked on css branch instead of new JS branch

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # proj_solo_daily-journal
+
+To save and view entries using the API, setup json-server with the following command, in the api directory:
+json-server -w entries.json -p 8088
--- scripts/JournalEntryList.js
+++ scripts/JournalEntryList.js
@@ -1,7 +1,6 @@
 import { useJournalEntries } from "./JournalDataProvider.js"
 import { JournalEntryComponent } from "./JournalEntry.js"
 
-
 export const EntryListComponent = () => {
     const domTarget = document.querySelector(".test-entries")
     // Use the journal entry data from the data provider component
