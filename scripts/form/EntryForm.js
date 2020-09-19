b5ce9f818bb1bb5beb1d0e6010c692db372d8794
ste163 <samtedwards@pm.me>
2020-09-19 17:44:59 -0400

    modal shows, not functioning

--- scripts/form/EntryForm.js
+++ scripts/form/EntryForm.js
@@ -3,7 +3,6 @@
 import { saveEntry } from "../data-providers/EntryProvider.js"
 import { getMoods, useMoods } from "../data-providers/MoodProvider.js"
 import { getConcepts, useConcepts } from "../data-providers/ConceptProvider.js"
-import { slideAnim } from "../animations/anim_slide.js";
 
 const eventHub = document.querySelector(".gridMain");
 const entryDOMtarget = document.querySelector(".sp-form");
@@ -62,46 +61,47 @@ const renderEntryForm = () => {
         const allMoods = useMoods()
         const allConcepts = useConcepts();
         return entryDOMtarget.innerHTML = `
-        <button class="sp-form__back-btn button__slider" type="button">&#128896</button>
-        <h2 class="sp-form__h2">Create a new Spiral</h2>
-        <form class="new" action="">
-            <fieldset class="new__center">
-               <label class="new__label new__label--small" for="s-author">Author</label>
-               <textarea class="new__author--text" name="s-author" rows="1" placeholder="Who are you?"></textarea>
-            </fieldset>
-            <fieldset class="new__center">
-                <label class="new__label new__label--small" for="s-mood">How's your Mood?</label>
-                <select class="new__mood--dropdown" id="s-mood" name="s-mood">
-                    <option value="select">Select an option</option>
+        <div class="modal">
+            <button class="sp-form__back-btn" type="button">&#128896</button>
+            <h2 class="sp-form__h2">Create a new Spiral</h2>
+            <form class="new" action="">
+                <fieldset class="new__center">
+                <label class="new__label new__label--small" for="s-author">Author</label>
+                <textarea class="new__author--text" name="s-author" rows="1" placeholder="Who are you?"></textarea>
+                </fieldset>
+                <fieldset class="new__center">
+                    <label class="new__label new__label--small" for="s-mood">How's your Mood?</label>
+                    <select class="new__mood--dropdown" id="s-mood" name="s-mood">
+                        <option value="select">Select an option</option>
+                        ${
+                            allMoods.map((mood) => {
+                                return `<option value="${mood.id}">${mood.label}</option>`
+                            }).join("")
+                        }
+                    </select>
+                </fieldset>
+                <fieldset class="new__center">
+                    <label class="new__label" for="s-concept" id="s-concept" name="s-concept">Concepts Covered</label>
+                    <div class="concepts">
                     ${
-                        allMoods.map((mood) => {
-                            return `<option value="${mood.id}">${mood.label}</option>`
+                        allConcepts.map((concept) => {
+                            return `<label class="concept__checkbox-label" for="concept--${concept.id}">${concept.label}</label>
+                            <input type="checkbox" class="concept__checkbox" id="concept--${concept.id}" name="${concept.label}" value="${concept.label}">`
                         }).join("")
                     }
-                </select>
-            </fieldset>
-            <fieldset class="new__center">
-                <label class="new__label" for="s-concept" id="s-concept" name="s-concept">Concepts Covered</label>
-                <div class="concepts">
-                ${
-                    allConcepts.map((concept) => {
-                        return `<label class="concept__checkbox-label" for="concept--${concept.id}">${concept.label}</label>
-                        <input type="checkbox" class="concept__checkbox" id="concept--${concept.id}" name="${concept.label}" value="${concept.label}">`
-                    }).join("")
-                }
-                </div>
-            </fieldset>
-            <fieldset class="new__center">
-                    <label class="new__label" for="s-title" id="s-title" name="s-title">Title</label>
-                    <textarea class="new__title--text" name="s-title" rows="2" placeholder="Forms are great!"></textarea>
-            </fieldset>
-            <fieldset class="new__center">
-                    <label class="new__label" for="s-text" id="s-text" name="s-text">Text</label>
-                    <textarea class="new__text--text" name="s-text" rows="8" placeholder="You can save all sorts of data."></textarea>
-            </fieldset>
-            <button type="button" id="new__save-btn" class="new__save-btn">Save Spiral</button>
-        </form>
+                    </div>
+                </fieldset>
+                <fieldset class="new__center">
+                        <label class="new__label" for="s-title" id="s-title" name="s-title">Title</label>
+                        <textarea class="new__title--text" name="s-title" rows="2" placeholder="Forms are great!"></textarea>
+                </fieldset>
+                <fieldset class="new__center">
+                        <label class="new__label" for="s-text" id="s-text" name="s-text">Text</label>
+                        <textarea class="new__text--text" name="s-text" rows="8" placeholder="You can save all sorts of data."></textarea>
+                </fieldset>
+                <button type="button" id="new__save-btn" class="new__save-btn">Save Spiral</button>
+            </form>
+        </div>
         `
     })
-    .then(slideAnim)
 }
