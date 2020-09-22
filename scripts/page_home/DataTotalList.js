1f3843cccbe59dcd5b29bf614dd11f083dfdf790
ste163 <samtedwards@pm.me>
2020-09-22 09:51:56 -0400

    Pulled out the data total sections

--- scripts/page_home/DataTotalList.js
+++ scripts/page_home/DataTotalList.js
@@ -1,5 +1,6 @@
-import {useEntries, getEntries } from "../data-providers/EntryProvider.js"
+import { useEntries, getEntries } from "../data-providers/EntryProvider.js"
 import { DataTotalHTML } from "./DataTotalHTML.js"
+import { DataMoodHTML } from "./DataMoodHTML.js"
 const eventHub = document.querySelector(".gridBody");
 
 // After user saves new form, update the total.
@@ -8,19 +9,56 @@ eventHub.addEventListener("entryStateChanged", e => {
     .then(() => {
         const entries = useEntries();
         totalRenderer(entries);
+        mostMoodRenderer(entries);
     })
 })
 
 export const listDataTotal = () => {
     getEntries()
     .then(() => {
-        totalRenderer(useEntries());
+        const allEntries = useEntries();
+        totalRenderer(allEntries)
+        mostMoodRenderer(allEntries);
     })
 
 }
 
 const totalRenderer = (entryArray) => {
-    const domTarget = document.querySelector(".sp-data")
     let totalHTML = DataTotalHTML(entryArray);
+    const domTarget = document.querySelector(".sp-total")
     domTarget.innerHTML = totalHTML;
+}
+
+
+const mostMoodRenderer = (entryArray) => {
+    // find the most used mood, then pass than into the HTML
+    const mode = findMode(entryArray)
+    console.log(mode)
+    let moodHTML = DataMoodHTML();
+    const domTarget = document.querySelector(".sp-average")
+    domTarget.innerHTML = moodHTML;
+    // take the entry array, find the most used mood,
+    // take that id, then display the mood's name.
+}
+
+// Pulled in a person's mode script, need to go through the selected moods
+const findMode = (array) => {
+    const map = new Map();
+    let maxFreq = 0;
+    let mode;
+    
+
+    for(const item of array) {
+      let freq = map.has(item) ? map.get(item) : 0;
+      freq++;
+  
+      if(freq > maxFreq) {
+        maxFreq = freq;
+        mode = item;
+      }
+      
+      map.set(item, freq);
+    }
+  
+    return mode;
 }
