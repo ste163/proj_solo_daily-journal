a874fae87f26715384868b2b1bac8847ae689682
ste163 <samtedwards@pm.me>
2020-09-22 16:52:46 -0400

    moved create button to nav

--- scripts/navBar.js
+++ scripts/navBar.js
@@ -1,16 +1,23 @@
 // Module goals: add event listeners to grow SVG underline
 const eventHub = document.querySelector(".gridBody")
-const targetElement = document.querySelector(".nav__underline")
+const viewAllTarget = document.querySelector(".nav__underline")
+const createTarget = document.querySelector(".nav__underline--button")
 
 eventHub.addEventListener("mouseover", e => {
     if (e.target.id === "navLink") {
-        targetElement.classList.add("nav__underline--grow")
-    } 
+        viewAllTarget.classList.add("nav__underline--grow")
+    }
+    if (e.target.id === "modalOpen") {
+        createTarget.classList.add("nav__underline--button--grow")
+    }
 })
 
 eventHub.addEventListener("mouseout", e => {
     if (e.target.id === "navLink") {
-        targetElement.classList.remove("nav__underline--grow")
+        viewAllTarget.classList.remove("nav__underline--grow")
+    }
+    if (e.target.id === "modalOpen") {
+        createTarget.classList.remove("nav__underline--button--grow")
     } 
 })
 
