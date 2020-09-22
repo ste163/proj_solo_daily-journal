baeb0dbce4cd4f0e8b0418c7afcc913d75662e2d
ste163 <samtedwards@pm.me>
2020-09-22 17:44:05 -0400

    changed most grid to flex box

--- scripts/page_view-all/view-all_main.js
+++ scripts/page_view-all/view-all_main.js
@@ -1,7 +1,9 @@
 import { initiateNavBar } from "../navBar.js"
 import { listEntryAll } from "./EntryAllList.js";
 import { FilterBar } from "./FilterBar.js";
+import { listEntryForm } from "../form/EntryForm.js"
 
+listEntryForm();
 initiateNavBar();
 listEntryAll();
 FilterBar();
