8f816e9d1b8949bcf9cd633b0b0043977d3c0a81
ste163 <samtedwards@pm.me>
2020-09-21 20:57:37 -0400

    added basic layout and styling to view all page

--- scripts/page_view-all/view-all_main.js
+++ scripts/page_view-all/view-all_main.js
@@ -1,5 +1,7 @@
+import { initiateNavBar } from "../navBar.js"
 import { getEntries } from "../data-providers/EntryProvider.js"
 import { listEntryAll } from "./EntryAllList.js";
 
+initiateNavBar();
 getEntries();
 listEntryAll();
